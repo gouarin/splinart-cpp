f07b3dd50db50c4517019f47fa51085017599ee8
Loic Gouarin <loic.gouarin@gmail.com>
2023-12-03 11:33:53 +0100

    chore: publish (#20)

--- conda/recipe/build.sh
+++ conda/recipe/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cmake -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX -S . -G Ninja -B build
+cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release -S . -G Ninja -B build
 ninja -C build install
