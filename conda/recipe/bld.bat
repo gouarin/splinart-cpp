dd9acfc7afa73b92fd371506449c4998a01a5a33
Loic Gouarin <loic.gouarin@gmail.com>
2023-12-08 07:10:56 +0100

    add windows

--- conda/recipe/bld.bat
+++ conda/recipe/bld.bat
@@ -1,7 +1,4 @@
-cmake -S . -G "Ninja" -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D CMAKE_BUILD_TYPE=Release -B build ^
-   -DOpenCV_DIR=%LIBRARY_PREFIX% ^
-   -DOpenCV_LIB_PATH=%LIBRARY_LIB% ^
-   -DOpenCV_INCLUDE_DIRS=%LIBRARY_INC%
+cmake -S . -G "Ninja" -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D CMAKE_BUILD_TYPE=Release -B build
 if errorlevel 1 exit 1
 
 ninja -C build install
