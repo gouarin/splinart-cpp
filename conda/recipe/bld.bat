99569370f58394e07518b0d8fc37bc8477d2ee39
Loic Gouarin <loic.gouarin@gmail.com>
2025-01-27 14:32:48 +0100

    add windows support

--- conda/recipe/bld.bat
+++ conda/recipe/bld.bat
@@ -1,6 +1,5 @@
-@REM cmake -S . -G "Ninja" -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D CMAKE_BUILD_TYPE=Release -B build
-cmake -S . -G "Visual Studio 17 2022" -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D CMAKE_BUILD_TYPE=Release -B build
+cmake %CMAKE_ARGS% -G "Ninja" -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% %SRC_DIR% -B build -D CMAKE_SYSTEM_PROCESSOR=x64
 if errorlevel 1 exit 1
 
-ninja -C build install
+cmake --build build --target install
 if errorlevel 1 exit 1
